37bf7c955a7ebc1eb04930b2f3e6f557ca601187
GeorgeLincoln <georgelincolnifce@gmail.com>
2018-04-25 00:22:20 -0300

    Add files via upload

--- AquaSys/ClassLibraries/Repositorios/RepositoryBase.cs
+++ AquaSys/ClassLibraries/Repositorios/RepositoryBase.cs
@@ -9,23 +9,10 @@ namespace ClassLibraries.Repositorios
     {
         private readonly DataContext _context;
 
-        public RepositoryBase(DataContext context, bool saveChanges = true)
+        public RepositoryBase(DataContext context)
         {
             this._context = context;
         }
-
-        public virtual void Save()
-        {
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-        }
-
         public virtual void Add(T entidade)
         {
             _context.Set<T>().Add(entidade);
@@ -37,19 +24,19 @@ namespace ClassLibraries.Repositorios
             return _context.Set<T>().ToList();
         }
 
-        public virtual T Find(int id)
+        public virtual T Find(long id)
         {
             return _context.Set<T>().FirstOrDefault(c => c.Id == id);
         }
 
-        public virtual void Remove(int id)
+        public virtual void Remove(long id)
         {
             var entidade = _context.Set<T>().First(c => c.Id == id);
             _context.Set<T>().Remove(entidade);
             _context.SaveChanges();
         }
 
-        public virtual void Update(T entidade, int id)
+        public virtual void Update(T entidade)
         {
             _context.Set<T>().Update(entidade);
         }
